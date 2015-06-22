f6d0f3e30c857c2ec6c425ed78181d055d88efe7
jeanfredrik <jeanfredrik@gmail.com>
2015-06-22 15:28:24 +0200

    bump and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jeanfredrik:denormalize',
-  version: '0.6.0',
+  version: '0.7.0',
   summary: 'Provides simple methods for common denormalization tasks',
   git: 'https://github.com/jeanfredrik/meteor-denormalize.git',
   documentation: 'README.md'
